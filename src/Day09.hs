6bf6190ca175076b56a325280b3b853274f811aa
Matthew Green <mgreen89@gmail.com>
2020-12-15 10:27:31 +0000

    Day 9 part a

--- src/Day09.hs
+++ src/Day09.hs
@@ -4,10 +4,35 @@ module Day09
   )
 where
 
--- I really want vectors... ugh, guess it's time to move off repl.it.
+import Debug.Trace (trace)
 
-day9a :: String -> Int
-day9a input = 1
+import           Data.List                      ( tails )
+import           Data.Maybe                     ( listToMaybe )
+import           Control.Monad                  ( guard )
 
-day9b :: String -> Int
-day9b input = 2
+parse :: String -> [Int]
+parse = fmap read . lines
+
+-- Returns 'Just val' if 'val' from the end of the list invalid.
+isInvalid :: [Int] -> Maybe Int
+isInvalid xs =
+  let tgt : srcs   = reverse xs
+      invalidCheck = do
+        y : ys <- tails srcs
+        z      <- ys
+        guard $ (y + z) == tgt
+  in  if null invalidCheck then Just tgt else Nothing
+
+findInvalid :: [Int] -> Maybe Int
+findInvalid vals = listToMaybe
+  [ y | ys <- tails vals, Just y <- [isInvalid (take 26 ys)] ]
+
+day9a :: String -> Maybe Int
+day9a = findInvalid . parse
+
+day9b :: String -> Maybe Int
+day9b input = do
+  let vals = parse input
+  invalid <- findInvalid vals
+  --
+  Nothing
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,36 +1,70 @@
-module Lib (libmain) where
+module Lib
+  ( libmain
+  )
+where
 
-import Day01 (day1a, day1b)
-import Day02 (day2a, day2b)
-import Day03 (day3a, day3b)
-import Day04 (day4a, day4b)
-import Day05 (day5a, day5b)
-import Day06 (day6a, day6b)
-import Day07 (day7a, day7b)
-import Day08 (day8a, day8b)
-import Day09 (day9a, day9b)
-import Day10 (day10a, day10b)
-import Day11 (day11a, day11b)
-import Day12 (day12a, day12b)
-import Day13 (day13a, day13b)
-import Day14 (day14a, day14b)
+import           Day01                          ( day1a
+                                                , day1b
+                                                )
+import           Day02                          ( day2a
+                                                , day2b
+                                                )
+import           Day03                          ( day3a
+                                                , day3b
+                                                )
+import           Day04                          ( day4a
+                                                , day4b
+                                                )
+import           Day05                          ( day5a
+                                                , day5b
+                                                )
+import           Day06                          ( day6a
+                                                , day6b
+                                                )
+import           Day07                          ( day7a
+                                                , day7b
+                                                )
+import           Day08                          ( day8a
+                                                , day8b
+                                                )
+import           Day09                          ( day9a
+                                                , day9b
+                                                )
+import           Day10                          ( day10a
+                                                , day10b
+                                                )
+import           Day11                          ( day11a
+                                                , day11b
+                                                )
+import           Day12                          ( day12a
+                                                , day12b
+                                                )
+import           Day13                          ( day13a
+                                                , day13b
+                                                )
+import           Day14                          ( day14a
+                                                , day14b
+                                                )
 
 
-import Control.Monad ((<=<), when)
-import Control.Monad.Fail (MonadFail)
-import Data.Maybe (mapMaybe)
-import System.Environment (getArgs)
-import Text.Printf
-import Text.Read (readMaybe)
+import           Control.Monad                  ( (<=<)
+                                                , when
+                                                )
+import           Control.Monad.Fail             ( MonadFail )
+import           Data.Maybe                     ( mapMaybe )
+import           System.Environment             ( getArgs )
+import           Text.Printf
+import           Text.Read                      ( readMaybe )
 
 getDayInput :: Int -> IO String
-getDayInput i =
-  readFile filename
-  where
-    filename = printf "inputs/day%0.2d.txt" i
+getDayInput i = readFile filename
+  where filename = printf "inputs/day%0.2d.txt" i
 
 checkRight :: MonadFail m => Either String a -> m a
 checkRight = either fail return
+
+checkJust :: MonadFail m => Maybe a -> m a
+checkJust = maybe (fail "Failed!") return
 
 runDay :: Int -> (a -> IO ()) -> [String -> a] -> IO ()
 runDay day output parts = do
@@ -43,17 +77,17 @@ runDay day output parts = do
 
 libmain :: IO ()
 libmain = do
-  runDay 1 print [ day1a, day1b ]
-  runDay 2 (print <=< checkRight) [ day2a, day2b ]
-  runDay 3 print [ day3a, day3b ]
-  runDay 4 id [ day4a, day4b ] -- Not done day 4 (not really my jam)
-  runDay 5 (print <=< checkRight) [ day5a, day5b ]
-  runDay 6 print [ day6a, day6b ]
-  runDay 7 (print <=< checkRight) [ day7a, day7b ]
-  runDay 8 (print <=< checkRight) [ day8a, day8b ]
-  runDay 9 print [ day9a, day9b ] -- Not done (vectors)
-  runDay 10 print [ day10a, day10b ]
-  runDay 11 print [ day11a, day11b ] -- (takes a while on repl.it)
-  runDay 12 print [ day12a, day12b ]
-  runDay 13 (print <=< checkRight) [ day13a, day13b ]
-  runDay 14 (print <=< checkRight) [ day14a, day14b ]
+  runDay 1  print                  [day1a, day1b]
+  runDay 2  (print <=< checkRight) [day2a, day2b]
+  runDay 3  print                  [day3a, day3b]
+  runDay 4  id                     [day4a, day4b] -- Not done day 4 (not really my jam)
+  runDay 5  (print <=< checkRight) [day5a, day5b]
+  runDay 6  print                  [day6a, day6b]
+  runDay 7  (print <=< checkRight) [day7a, day7b]
+  runDay 8  (print <=< checkRight) [day8a, day8b]
+  runDay 9  (print <=< checkJust)  [day9a, day9b]
+  runDay 10 print                  [day10a, day10b]
+  runDay 11 print                  [day11a, day11b]
+  runDay 12 print                  [day12a, day12b]
+  runDay 13 (print <=< checkRight) [day13a, day13b]
+  runDay 14 (print <=< checkRight) [day14a, day14b]
